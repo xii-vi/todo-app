d13574e75bc533266ddbd5851942ef6cc30fd972
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2021-11-02 08:48:43 +0530

    Compacted the code
    made code more efficient.

--- index.js
+++ index.js
@@ -2,29 +2,24 @@ var itemAdd = document.getElementById('entertask');
 var btnAdd = document.getElementById('additem');
 var ulList = document.getElementById('listItem')
 
-btnAdd.addEventListener('click', () => {
-    if (itemAdd.value === "") {
-        alert("Please Enter the task first to add to TO-DO List")
-    } else {
+function addListItem() {
+    if (itemAdd.value !== "") {
         var newListElement = document.createElement('li');
         var textNode = document.createTextNode(itemAdd.value);
         ulList.appendChild(newListElement);
         newListElement.appendChild(textNode);
         newListElement.id = "item" + ulList.childElementCount;
         newListElement.classList.add("todo-list");
+    } else {
+        alert("Please Enter the task first to add to TO-DO List")
+    }
+}
+
+itemAdd.addEventListener('keyup', function (event) {
+    var keyPressed = event.key;
+    if (keyPressed === "Enter") {
+        addListItem();
     }
 })
 
-itemAdd.addEventListener('keydown', function (event) {
-    var keyPressed = event.key;
-    if (keyPressed === "Enter" && itemAdd.value === "") {
-        alert("Please Enter the task first to add to TO-DO List")
-    } else if (keyPressed === "Enter") {
-        var newListElement = document.createElement('li');
-        var textNode = document.createTextNode(itemAdd.value);
-        ulList.appendChild(newListElement);
-        newListElement.appendChild(textNode);
-        newListElement.id = "item" + ulList.childElementCount;
-        newListElement.classList.add("todo-list");
-    }
-})
+btnAdd.addEventListener('click', addListItem)
